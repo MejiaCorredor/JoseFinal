eead756523ff576a5b34f9edd1af81778236a3fa
MejiaCorredor <jose.mejiac@upb.edu.co>
2023-11-17 15:58:46 -0500

    JoseFinal

--- src/JoseFinal.java
+++ src/JoseFinal.java
@@ -1,82 +1,104 @@
-import java.io.BufferedReader;
-import java.io.File;
-//import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import java.util.List;
-//import java.util.Scanner
+import java.io.*;
+import java.util.*;
 
 public class JoseFinal {
+    public static void main(String[] args) throws IOException {
 
-    public class transacciones {
+        String[] dias = { "lunes.txt", "martes.txt", "miercoles.txt", "jueves.txt", "viernes.txt" };
+        HashMap<String, ArrayList<String[]>> datosSemana = new HashMap<>();
 
-        int id;
-        int monto;
-        int tipoE;
-        int tipoR;
-        int hora;
-
-    }
-
-    public static String[][] Dias(String fileN) {
-
-        String[][] resultados = new String[100][5];
-
-        File archivo = null;
-        FileReader fr = null;
-        BufferedReader br = null;
-
-        try {
-
-            archivo = new File("jueves.txt", "domingo.txt");
-            fr = new FileReader(archivo);
-            br = new BufferedReader(fr);
-
-            String linea;
-
-            while ((linea = br.readLine()) != null) {
-                for (int j = 0; j < 100; j++) {
-                    String[] split = linea.split(";");
-                    resultados[j] = split;
-                    linea = br.readLine();
-                }
-
+        for (String dia : dias) {
+            BufferedReader reader = new BufferedReader(new FileReader(dia));
+            ArrayList<String[]> datosDia = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                datosDia.add(line.split(";"));
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Error al leer el archivo");
-        } finally {
-            try {
-                if (null != fr) {
-                    fr.close();
-                }
-            } catch (Exception e2) {
-                e2.getMessage();
+            datosSemana.put(dia, datosDia);
+            reader.close();
+        }
+
+        Scanner leer = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Día de la semana cuando mayor cantidad de dinero se movió");
+            System.out.println("2. Hora del día en que más cantidad de dinero se mueve en promedio");
+            System.out.println("3. Transacción a través ID");
+            System.out.println("4. Los datos n de X día");
+            System.out.println("5. Salir");
+            System.out.print("Elige una opción: ");
+            int option = leer.nextInt();
+
+            if (option == 5) {
+                break;
+            }
+
+            switch (option) {
+                case 1:
+                    String maximoDia = "";
+                    int monto = 0;
+                    for (String dia : datosSemana.keySet()) {
+                        int totalmonto = 0;
+                        for (String[] transaccion : datosSemana.get(dia)) {
+                            totalmonto += Integer.parseInt(transaccion[1]);
+                        }
+                        if (totalmonto > monto) {
+                            monto = totalmonto;
+                            maximoDia = dia;
+                        }
+                    }
+
+                    System.out.println("día de la semana en que mayor cantidad de dinero se manejo: " + maximoDia);
+                    break;
+                case 2:
+                    int[] horas = new int[24];
+                    for (String dia : datosSemana.keySet()) {
+                        for (String[] transaccion : datosSemana.get(dia)) {
+                            horas[Integer.parseInt(transaccion[4])] += Integer.parseInt(transaccion[1]);
+                        }
+                    }
+                    int maxHora = 0;
+                    for (int i = 0; i < 24; i++) {
+                        if (horas[i] > horas[maxHora]) {
+                            maxHora = i;
+                        }
+                    }
+                    System.out.println(
+                            "hora del día en que más cantidad de dinero se mueve en promedio de los siete días: "
+                                    + maxHora);
+                    break;
+                case 3:
+                    System.out.print("id de la transacción: ");
+                    String id = leer.next();
+                    for (String dia : datosSemana.keySet()) {
+                        for (String[] transaccion : datosSemana.get(dia)) {
+                            if (transaccion[0].equals(id)) {
+                                System.out.println("id transacción: " + transaccion[0]);
+                                System.out.println("cantidad de dinero enviado: " + transaccion[1]);
+                                System.out.println("tipo emisor: " + transaccion[2]);
+                                System.out.println("tipo receptor: " + transaccion[3]);
+                                System.out.println("hora de la transacción: " + transaccion[4]);
+                            }
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.print("ingresa el día: ");
+                    String dia = leer.next();
+                    System.out.print("Ingrese el número de transacciones a visualizar: ");
+                    int n = leer.nextInt();
+                    for (int i = 0; i < n; i++) {
+                        String[] transaccion = datosSemana.get(dia).get(i);
+                        System.out.println("id transacción: " + transaccion[0]);
+                        System.out.println("cantidad de dinero enviado: " + transaccion[1]);
+                        System.out.println("tipo de emisor: " + transaccion[2]);
+                        System.out.println("tipo de receptor: " + transaccion[3]);
+                        System.out.println("hora de la transacción: " + transaccion[4]);
+                    }
+                    break;
+                default:
+                    System.out.println("no es válida.");
             }
         }
-        return resultados;
-
-    }
-
-    public static void main(String[] args) {
-
-        HashMap<String, List<String>> mapa = new HashMap<>();
-
-        List<String> size = new ArrayList<>();
-        size.add("id");
-        size.add("monto");
-        size.add("tipoE");
-        size.add("tipoR");
-        size.add("hora");
-
-
-        mapa.size(); // Tamaño del mapa
-
-        for (String element : mapa.keySet()) {
-
-            System.out.println(element + " sacó " + mapa.get(element));
-        }
+        leer.close();
     }
 }
